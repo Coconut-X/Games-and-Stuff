f534f42a05a55d9acfd3db5e10f203ad2748ffca
Coconut-X <144850519+Coconut-X@users.noreply.github.com>
2024-02-08 23:31:23 +0500

    Update Bouncing Ball Simulation.cpp
    Added sound effect

--- Bouncing Ball Simulation.cpp
+++ Bouncing Ball Simulation.cpp
@@ -1,7 +1,7 @@
 #include "raylib.h"
 #include<string>
 #include<math.h>
-#include<iostream>
+
 
 float log_base(float x, float base)
 {
@@ -17,41 +17,54 @@ int main()
     const float initialHeight = 100;
     const float coefficientOfRestitution = 0.9f;
 
-    SetTargetFPS(60);
-    InitWindow(screenHeight, screenHeight, "Bouncing Ball");
     float posX = screenWidth / 2.0f;
     float posY = initialHeight;
     float velocityY = 0;
     int bounce = 0;
     int totalBounces=log_base(1/initialHeight,coefficientOfRestitution);
+
+    SetTargetFPS(60);
+    InitWindow(screenHeight, screenHeight, "Bouncing Ball");
     Color color = RED;
+    
+    InitAudioDevice();
+    Wave wave = LoadWave("ballBounce.wav");
+    Sound sound = LoadSound("ballBounce.wav");
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        std::string text = std::to_string(bounce);
-        SetTargetFPS(60);
+        color = RED;
 
+        std::string text = std::to_string(bounce);
         DrawText(text.c_str(), 20, 20, 50, GREEN);
-      
+        
         velocityY += 0.5f;
         posY += velocityY;
 
-        color = RED;
         if ((posY + ballRadius +2 >= screenHeight))
         {
             ClearBackground(ORANGE);
+            PlaySound(sound);
+
             if (bounce <= totalBounces) bounce++;
+
             posY = screenHeight - ballRadius;
             velocityY *= -coefficientOfRestitution;
             color = GREEN;
         }
+
         DrawLine(0, screenHeight-1, screenWidth, screenHeight-1, color);
         DrawLine(0, screenHeight, screenWidth, screenHeight, color);
+
         DrawCircle(posX, posY, ballRadius, DARKPURPLE);
        
         EndDrawing();
     }
+
+    UnloadWave(wave);
+    UnloadSound(sound);
 
     CloseWindow();
     return 0;
